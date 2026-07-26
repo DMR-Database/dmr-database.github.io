4a94a7833a0ca6a37323d6b5185de0787b8da34e
Albert Einstein <javastraat@hotmail.com>
2026-07-26 14:56:13 +0200

    *Uploaded by PD2EMC : new release

--- webapp/flutter_service_worker.js
+++ webapp/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon-16x16.png": "223decb4adccf669729d3990c8a40095",
-"flutter_bootstrap.js": "17334eda4052d5dd98f82e8056a0a8d0",
+"flutter_bootstrap.js": "64e0ae6f76e05deb18556a14768094a7",
 "version.json": "f8330449115b5a683c53ca59b4b0afb9",
 "splash/img/light-2x.png": "ee96fe3a76341755cd1957234d7b633a",
 "splash/img/dark-4x.png": "3edf0c85383db4fc7209eaa10567f9d9",

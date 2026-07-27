2a8ea908afa89760e524a458b7dddfd4449a7e4c
Albert Einstein <javastraat@hotmail.com>
2026-07-27 17:31:21 +0200

    *Uploaded by PD2EMC : new release

--- webapp/flutter_service_worker.js
+++ webapp/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon-16x16.png": "223decb4adccf669729d3990c8a40095",
-"flutter_bootstrap.js": "1b80fc0920816dc38c8bf7caa11454bc",
+"flutter_bootstrap.js": "9680e62e482fb4171f0470fb77c2b612",
 "version.json": "84ee284e1dface04a55dd0206dd78e42",
 "splash/img/light-2x.png": "ee96fe3a76341755cd1957234d7b633a",
 "splash/img/dark-4x.png": "3edf0c85383db4fc7209eaa10567f9d9",
@@ -20,7 +20,7 @@ const RESOURCES = {"favicon-16x16.png": "223decb4adccf669729d3990c8a40095",
 "/": "1f3c8122f27e2a9569f5f14f45ca1554",
 "android-chrome-192x192.png": "1eb2daaf58f74a44a5d283ba43a62e29",
 "apple-touch-icon.png": "829ad2aec2acdada14e8feedb34740bc",
-"main.dart.js": "8371b85c5c78a93223a0c66213d4f4bc",
+"main.dart.js": "b5e27944cf4cc9ed089b254ca39d1298",
 "sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
